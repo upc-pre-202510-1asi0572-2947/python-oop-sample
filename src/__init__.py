bfb77dc5ce8ad2867b4f2d013bdee30c41da8058
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-11 10:46:17 -0700

    docs: add code-level documentation to init.

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+File: __init__.py
+Purpose: Package initialization for the src module in python-oop-sample
+Author: IoT Solution Development Team
+Version: 0.1.0
+Date: 2025-04-11
+License: MIT
+
+Copyright (c) 2025 IoT Solution Development Team
+
+"""
